002d64d5bfc7deef6ec521ba3e80bab9b1da5edd
SaifAliRko <saif.ali96587@gmail.com>
2025-11-13 07:29:03 +0100

    bug fix: Fixed Searchbar related issues

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,21 @@
-"use client";
+'use client';
 
-import type { RootState } from "@/store";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  ClearButton,
-  InputWrapper,
-  SearchInput,
-  SearchWrapper,
-} from "./SearchBar.styles";
+import type { RootState } from '@/store';
+import { setSearchQuery } from '@/store/slices/uiSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { ClearButton, InputWrapper, SearchInput, SearchWrapper } from './SearchBar.styles';
 
 export function SearchBar() {
   const dispatch = useDispatch();
   const searchQuery = useSelector((state: RootState) => state.ui.searchQuery);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchQuery(e.target.value));
+  };
+
+  const handleClear = () => {
+    dispatch(setSearchQuery(''));
+  };
 
   return (
     <SearchWrapper>
@@ -20,9 +24,9 @@ export function SearchBar() {
           type="text"
           placeholder="Search albums by name, artist..."
           value={searchQuery}
-          onChange={() => {}}
+          onChange={handleChange}
         />
-        {searchQuery && <ClearButton onClick={() => {}}>✕</ClearButton>}
+        {searchQuery && <ClearButton onClick={handleClear}>✕</ClearButton>}
       </InputWrapper>
     </SearchWrapper>
   );
